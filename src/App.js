4436fe621f0bca3e9029b93f6969b87cd4df31be
olzzon85 <olsson22@hotmail.com>
2022-10-18 11:32:45 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ContactsView from './views/ContactsView'
+import HomeView from './views/HomeView'
+import NotFoundView from './views/NotFoundView'
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"element={<HomeView/>} />
+        <Route path="/contacts"element={<ContactsView/>} />
+        <Route path="*"element={<NotFoundView/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
